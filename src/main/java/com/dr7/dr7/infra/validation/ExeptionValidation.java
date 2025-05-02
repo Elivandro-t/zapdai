7d59d5cbbfd400c47794d11b73702e420f94adaa
elivandro <elivandro78@gmail.com>
2025-05-01 21:05:45 -0300

    adicionando validacao

--- src/main/java/com/dr7/dr7/infra/validation/ExeptionValidation.java
+++ src/main/java/com/dr7/dr7/infra/validation/ExeptionValidation.java
@@ -1,8 +1,12 @@
 package com.dr7.dr7.infra.validation;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class ExeptionValidation {
@@ -10,5 +14,10 @@ public class ExeptionValidation {
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<validationDto> ValidaExeption(RuntimeException e){
         return ResponseEntity.badRequest().body(new validationDto(e.getMessage()));
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<?> handleBadCredentials(BadCredentialsException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(Map.of("erro", "Usuário ou senha inválidos"));
     }
 }
